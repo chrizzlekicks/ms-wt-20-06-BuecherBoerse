e928cf8bca5e3a22c143d3c944a6d8977b6681b5
Chris Schimetschka <chris.schimetschka@gmail.com>
2021-08-15 21:08:46 +0200

    swapped kodebi logo with buecherregal logo, update imprint

--- client/src/pages/Imprint.js
+++ client/src/pages/Imprint.js
@@ -1,8 +1,8 @@
-import { useGlobalContext } from '../context/GlobalContext';
-import { motion } from 'framer-motion';
+import { useGlobalContext } from '../context/GlobalContext'
+import { motion } from 'framer-motion'
 
 const Imprint = () => {
-  const { closeSubmenu } = useGlobalContext();
+  const { closeSubmenu } = useGlobalContext()
   return (
     <>
       <motion.main
@@ -14,28 +14,26 @@ const Imprint = () => {
       >
         <section className='imprint'>
           <h3>Impressum</h3>
-          <p>Angaben gem. § 5 TMG </p>
-          <br />
+          <h4>Angaben gem. § 5 TMG </h4>
           <p>Betreiber und Kontakt:</p>
+          <br />
           <p>
             <strong>
-              <span>TechLabs e.V.</span>
+              <span>Kodebi Booksharing</span>
             </strong>
-          </p>
-          <p>
-            David Middelbeck
             <br />
-            Goebenstr. 14
+            Gabriel-Max-Str. 14
             <br />
-            48151 Münster
+            10245 Berlin
           </p>
-          <p>E-Mail-Adresse: info@techlabs.org</p>
+          <p>E-Mail-Adresse: info@kodebi.de</p>
           <br />
           <p>
-            Vorstand: David Middelbeck, Marius Vennemann, Rebecca Janßen, Jana
-            Jeggle, Eric Grunenberg
+            Verantwortlich für den Inhalt: Johannes Kellers, Lena Krause,
+            Maximilian Muhle, Christian Schimetschka
             <br />
-            Register und Registernummer: Amtsgericht Münster VR 5787
+            Register und Registernummer: Amtsgericht Berlin-Charlottenburg XY
+            XXX
           </p>
           <br />
           <p>
@@ -53,23 +51,83 @@ const Imprint = () => {
               http://ec.europa.eu/consumers/odr/
             </a>
           </p>
-
+          <br />
+          <h4>Haftungsausschluss – Disclaimer:</h4>
+          <p>Haftung für Inhalte</p>
+          <br />
           <p>
-            <br />
-            Quelle:{' '}
-            <a
-              href='https://www.techlabs.org/de/imprint'
-              target='blank'
-              className='extlink'
-            >
-              https://www.techlabs.org/de/imprint
-            </a>
+            Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt
+            und nach bestem Gewissen erstellt. Für die Richtigkeit,
+            Vollständigkeit und Aktualität der Inhalte können wir jedoch keine
+            Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG
+            für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+            verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
+            jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+            Informationen zu überwachen oder nach Umständen zu forschen, die auf
+            eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur
+            Entfernung oder Sperrung der Nutzung von Informationen nach den
+            allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche
+            Haftung ist jedoch erst ab dem Zeitpunkt der Kenntniserlangung einer
+            konkreten Rechtsverletzung möglich. Bei Bekanntwerden von den o.g.
+            Rechtsverletzungen werden wir diese Inhalte unverzüglich entfernen.
           </p>
           <br />
+          <p>Haftungsbeschränkung für externe Links</p>
+          <br />
+          <p>
+            Unsere Webseite enthält Links auf externe Webseiten Dritter. Auf die
+            Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir
+            keinen Einfluss. Daher können wir für die „externen Links” auch
+            keine Gewähr auf Richtigkeit der Inhalte übernehmen. Für die Inhalte
+            der externen Links sind die jeweilige Anbieter oder Betreiber
+            (Urheber) der Seiten verantwortlich. <br />
+            Die externen Links wurden zum Zeitpunkt der Linksetzung auf
+            eventuelle Rechtsverstöße überprüft und waren im Zeitpunkt der
+            Linksetzung frei von rechtswidrigen Inhalten. Eine ständige
+            inhaltliche Überprüfung der externen Links ist ohne konkrete
+            Anhaltspunkte einer Rechtsverletzung nicht möglich. Bei direkten
+            oder indirekten Verlinkungen auf die Webseiten Dritter, die
+            außerhalb unseres Verantwortungsbereichs liegen, würde eine
+            Haftungsverpflichtung ausschließlich in dem Fall nur bestehen, wenn
+            wir von den Inhalten Kenntnis erlangen und es uns technisch möglich
+            und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu
+            verhindern. Diese Haftungsausschlusserklärung gilt auch innerhalb
+            des eigenen Internetauftrittes „Name Ihrer Domain” gesetzten Links
+            und Verweise von Fragestellern, Blogeinträgern, Gästen des
+            Diskussionsforums. Für illegale, fehlerhafte oder unvollständige
+            Inhalte und insbesondere für Schäden, die aus der Nutzung oder
+            Nichtnutzung solcherart dargestellten Informationen entstehen,
+            haftet allein der Diensteanbieter der Seite, auf welche verwiesen
+            wurde, nicht derjenige, der über Links auf die jeweilige
+            Veröffentlichung lediglich verweist. Werden uns Rechtsverletzungen
+            bekannt, werden die externen Links durch uns unverzüglich entfernt.
+            Urheberrecht Die auf unserer Webseite veröffentlichen Inhalte und
+            Werke unterliegen dem deutschen Urheberrecht (
+            <a href='http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf'>
+              http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf
+            </a>
+            ). <br />
+            Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+            Verwertung des geistigen Eigentums in ideeller und materieller Sicht
+            des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der
+            vorherigen schriftlichen Zustimmung des jeweiligen Urhebers i.S.d.
+            Urhebergesetzes (
+            <a href='http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf'>
+              http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf
+            </a>
+            ). Downloads und Kopien dieser Seite sind nur für den privaten und
+            nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer
+            Webseite nicht von uns erstellt wurden, sind die Urheberrechte
+            Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich
+            gemacht. Sollten Sie trotzdem auf eine Urheberrechtsverletzung
+            aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+            Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte
+            unverzüglich entfernen.
+          </p>
         </section>
       </motion.main>
     </>
-  );
-};
+  )
+}
 
-export default Imprint;
+export default Imprint
