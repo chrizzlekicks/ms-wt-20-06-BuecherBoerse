4dde8c1dbebb2e14c407828aa81fe86e1880e6d4
Johannes <2518665+jkellers@users.noreply.github.com>
2021-08-24 14:07:24 +0200

    timestamps

--- server/models/conversation.model.js
+++ server/models/conversation.model.js
@@ -18,17 +18,14 @@ const ConversationSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
-    created: {
-        type: Date,
-        default: Date.now,
-    },
-    // BookId als betreff
-    updated: Date, // Last message
     group: {
         type: String,
         trim: true,
         lowercase: true
         //required: [true, 'Gruppe ist erforderlich']
+    },
+    {
+        timestamps: true
     }
 });
 
