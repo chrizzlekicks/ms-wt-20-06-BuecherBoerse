73c4466a22b229a2d8e156e29331d542f07631b4
Johannes Kellers <johannes.kellers@gmail.com>
2021-09-25 20:56:13 +0200

    Four tabs

--- server/express.js
+++ server/express.js
@@ -21,23 +21,27 @@ app.use(compress());
 
 // Secure apps
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-eval'", "'unsafe-inline'"],
-      styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
-      fontSrc: ['https://fonts.gstatic.com'],
-      imgSrc: ["'self'", 'https://ik.imagekit.io'],
-      baseUri: ["'self'"],
-    },
-  })
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-eval'", "'unsafe-inline'"],
+            styleSrc: [
+                "'self'",
+                'https://fonts.googleapis.com',
+                "'unsafe-inline'"
+            ],
+            fontSrc: ['https://fonts.gstatic.com'],
+            imgSrc: ["'self'", 'https://ik.imagekit.io'],
+            baseUri: ["'self'"]
+        }
+    })
 );
 // Cross Origin Resource Sharing
 app.use(cors());
 
 // Serve up static files when deployed
 if (config.env === 'production') {
-  app.use(express.static(path.join(CURRENT_WORKING_DIR, 'client/build')));
+    app.use(express.static(path.join(CURRENT_WORKING_DIR, 'client/build')));
 }
 
 // use morgan for logging
@@ -50,16 +54,16 @@ app.use('/api/books', bookRoutes);
 app.use('/api/messages', conversationRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(CURRENT_WORKING_DIR + '/client/build/index.html'));
-  if (err) {
-    res.status(500).send(err);
-  }
+    res.sendFile(path.join(CURRENT_WORKING_DIR + '/client/build/index.html'));
+    if (err) {
+        res.status(500).send(err);
+    }
 });
 
 app.use((err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).json({ error: err.name + ': ' + err.message });
-  }
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: err.name + ': ' + err.message });
+    }
 });
 
 export default app;
--- server/models/messages.model.js
+++ server/models/messages.model.js
@@ -1,32 +1,32 @@
 import mongoose from 'mongoose';
 
 const MessageSchema = new mongoose.Schema(
-  {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        reciever: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        message: {
+            type: String,
+            required: [true, 'Bitte Nachricht eingeben']
+        },
+        group: {
+            type: String,
+            trim: true,
+            lowercase: true
+            //required: [true, 'Gruppe ist erforderlich']
+        }
     },
-    reciever: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-    },
-    message: {
-      type: String,
-      required: [true, 'Bitte Nachricht eingeben'],
-    },
-    group: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      //required: [true, 'Gruppe ist erforderlich']
-    },
-  },
-  {
-    timestamps: {
-      createdAt: 'createdAt',
-      updatedAt: 'updatedAt',
-    },
-  }
+    {
+        timestamps: {
+            createdAt: 'createdAt',
+            updatedAt: 'updatedAt'
+        }
+    }
 );
 
 export default mongoose.model('Message', MessageSchema);
